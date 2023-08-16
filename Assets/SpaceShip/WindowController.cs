2f8e0ef873a9d43fa9f1e5f907523a51ce28ba3e
GalileuGalilei <alfredocossetin@outlook.com>
2023-08-15 21:37:51 -0300

    spaceship

--- Assets/SpaceShip/WindowController.cs
+++ Assets/SpaceShip/WindowController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class WindowController : MonoBehaviour
 {
+    [SerializeField]
+    private SpaceShipController[,] grid = new SpaceShipController[3,6];
+    private Camera camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;   
     }
 
     // Update is called once per frame
